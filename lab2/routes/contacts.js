023fcf211dbc9e97a4eeccd057cbd958492a265b
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-02-18 20:44:25 -0600

    render contacts page

--- lab2/routes/contacts.js
+++ lab2/routes/contacts.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('contacts', {title: 'Welcome to the Contacts Page!', contacts: data});
 });
 
 module.exports = router;
