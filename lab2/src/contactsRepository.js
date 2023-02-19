fe04321cf7e5929aad9025f8f2899116760f0599
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-02-18 21:16:08 -0600

    Create contacts_add

--- lab2/src/contactsRepository.js
+++ lab2/src/contactsRepository.js
@@ -7,3 +7,11 @@ const saveData = () => {
     const stringifyData = JSON.stringify(Array.from(db));
     fs.writeFileSync(path.join(__dirname, '../data/contacts.json'), stringifyData);
 };
+
+const loadData = () => {
+    const fileData = fs.readFileSync(path.join(__dirname, '../database/contacts.json'));
+    const contactsArray = JSON.parse(fileData);
+    contactsArray.forEach(element => {
+        database.set(element[0], element[1]);
+    });
+};
