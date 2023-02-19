99c43d15a1a9d63cacafdd6efe4410e234f1dd9a
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-02-18 21:23:56 -0600

    Create contact

--- lab2/routes/contacts.js
+++ lab2/routes/contacts.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const contactsRepo = require ('../src/contactsRepository');
+const { body, validationResult } = require ('express-validator');
 
 /* GET find all */
 router.get('/', function(req, res, next) {
@@ -11,6 +12,30 @@ router.get('/', function(req, res, next) {
 /* GET contact_add */
 router.get('/add', function(req, res, next) {
     res.render('contacts_add', { title: 'Create a new contact'});
+});
+
+/* POST - Create contact */
+router.post('/add',
+    body('firstName').trim().notEmpty().withMessage('Cannot be empty!'),
+    body('lastName').trim().notEmpty().withMessage('Cannot be empty!'),
+    body('email').trim().notEmpty().withMessage('Cannot be empty!').isEmail().withMessage('Please enter a valid email address!'),
+    body('notes').trim(),
+    function(req, res, next) {
+
+    const result = validationResult(req);
+    if (result.isEmpty() != true){
+        res.render('contacts_add', { title: 'Create a new contact', message: result.array() })
+    }
+    else{
+        contactsRepo.create({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            notes: req.body.notes,
+        });
+
+        res.redirect('/contacts');
+    }
 });
 
 module.exports = router;
