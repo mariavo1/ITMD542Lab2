ae06f073270434b5a60a11179206ccc330fae0ed
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-02-18 21:19:01 -0600

    repo function

--- lab2/src/contactsRepository.js
+++ lab2/src/contactsRepository.js
@@ -9,9 +9,38 @@ const saveData = () => {
 };
 
 const loadData = () => {
-    const fileData = fs.readFileSync(path.join(__dirname, '../database/contacts.json'));
+    const fileData = fs.readFileSync(path.join(__dirname, '../data/contacts.json'));
     const contactsArray = JSON.parse(fileData);
     contactsArray.forEach(element => {
-        database.set(element[0], element[1]);
+        db.set(element[0], element[1]);
     });
 };
+
+const repo = {
+    findAll: () => Array.from(database.values()),
+    findByID: (id) => db.get(id),
+    create: (contacts) => {
+        const newContact = {
+            id: crypto.randomUUID(),
+            firstName: contacts.firstName,
+            lastName: contacts.lastName,
+            email: contacts.email,
+            notes: contacts.notes,
+        };
+
+        db.set(newContact.id, newContact);
+        saveData();
+    },
+    deleteByID: (id) => {
+        db.delete(id);
+        saveData();
+    },
+    update: (contact) => {
+        db.set(contact.id, contact);
+        saveData();
+    },
+};
+
+loadData();
+
+module.exports = repo;
