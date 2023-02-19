2706a00079a57ded1ef010fea0ec89e7beb71617
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-02-18 20:51:49 -0600

    create contacts.json

--- lab2/routes/contacts.js
+++ lab2/routes/contacts.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const contactsRepo = require ('../src/contactsRepository');
+const { body, validationResult } = require ('express-validator');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('contacts', {title: 'Welcome to the Contacts Page!', contacts: data});
+    const data = contactsRepo.findAll();
+    res.render('contacts', {title: 'Welcome to the Contacts Page!', contacts: data});
 });
 
 module.exports = router;
