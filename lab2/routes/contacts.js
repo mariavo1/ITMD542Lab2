b4cc8e4e8aa1dd1e652ea7cab08cf342987ce0c4
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-02-18 21:20:42 -0600

    get contact add

--- lab2/routes/contacts.js
+++ lab2/routes/contacts.js
@@ -2,10 +2,15 @@ var express = require('express');
 var router = express.Router();
 const contactsRepo = require ('../src/contactsRepository');
 
-/* GET users listing. */
+/* GET find all */
 router.get('/', function(req, res, next) {
     const data = contactsRepo.findAll();
     res.render('contacts', {title: 'Welcome to the Contacts Page!', contacts: data});
+});
+
+/* GET contact_add */
+router.get('/add', function(req, res, next) {
+    res.render('contacts_add', { title: 'Create a new contact'});
 });
 
 module.exports = router;
