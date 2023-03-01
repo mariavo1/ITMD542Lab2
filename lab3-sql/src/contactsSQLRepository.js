4e7ca88799b6667b48081b3aec79619cada32622
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-02-28 19:53:22 -0600

    Comment out unused data

---
+++ lab3-sql/src/contactsSQLRepository.js
@@ -0,0 +1,51 @@
+const betterSqlite3 = require('better-sqlite3');
+const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
+
+const db = new sqlite(path.join(__dirname, '../data/contacts.sqlite'), {verbose: console.log});
+
+const createTable = db.prepare("CREATE TABLE IF NOT EXISTS CONTACTS (id INTEGER PRIMARY KEY AUTOINCREMENT, first_name TEXT NOT NULL, last_name TEXT NOT NULL, email TEXT NOT NULL, notes TEXT)");
+createTable.run();
+
+// const saveData = () => {
+//     const stringifyData = JSON.stringify(Array.from(db));
+//     fs.writeFileSync(path.join(__dirname, '../data/contacts.json'), stringifyData);
+// };
+
+// const loadData = () => {
+//     const fileData = fs.readFileSync(path.join(__dirname, '../data/contacts.json'));
+//     const contactsArray = JSON.parse(fileData);
+//     contactsArray.forEach(element => {
+//         db.set(element[0], element[1]);
+//     });
+// };
+
+const repo = {
+    findAll: () => Array.from(db.values()),
+    findByID: (id) => db.get(id),
+    create: (contacts) => {
+        const newContact = {
+            // id: crypto.randomUUID(),
+            // firstName: contacts.firstName,
+            // lastName: contacts.lastName,
+            // email: contacts.email,
+            // notes: contacts.notes,
+        };
+
+        // db.set(newContact.id, newContact);
+        // saveData();
+    },
+    deleteByID: (id) => {
+        // db.delete(id);
+        // saveData();
+    },
+    update: (contact) => {
+        // db.set(contact.id, contact);
+        // saveData();
+    },
+};
+
+// loadData();
+
+module.exports = repo;
