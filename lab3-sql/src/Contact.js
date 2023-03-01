aa21920e67390997247680422614954ce3f290b3
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-02-28 20:23:51 -0600

    Create class Contact

---
+++ lab3-sql/src/Contact.js
@@ -0,0 +1,11 @@
+class Contact{
+    constructor(id, first_name, last_name, email, notes) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.notes = notes;
+    }
+}
+
+module.exports = Contact;
