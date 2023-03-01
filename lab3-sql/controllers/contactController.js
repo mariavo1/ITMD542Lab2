4b1688a5a35ffc74ef25f2406a83275e880c497f
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-02-28 20:00:08 -0600

    POST add contacts

--- lab3-sql/controllers/contactController.js
+++ lab3-sql/controllers/contactController.js
@@ -10,3 +10,16 @@ exports.contacts_list = function(req, res, next) {
   exports.contacts_add_get = function(req, res, next) {
     res.render('contacts_add', { title: 'Create a new contact'});
 };
+
+exports.contacts_add_post = function(req, res, next) {
+  
+    const result = validationResult(req);
+    if (result.isEmpty() != true){
+        res.render('contacts_add', { title: 'Create a new contact', message: result.array() })
+    }
+    else{
+      const newContact = new Contact('', req.body.first_name, req.body.last_name, req.body.email, req.body.notes, '');
+      contactsSQLRepository.create(newContact);
+      res.redirect('/contacts');
+    }
+};
