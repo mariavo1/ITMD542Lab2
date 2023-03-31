2a39b6a570a297d8db3ebdab413ec5707495b149
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-03-31 14:23:13 -0500

    Upload copy of lab2

---
+++ lab4-mongo-atlas/src/contactMongoRepo.js
@@ -0,0 +1,78 @@
+const { MongoClient, ObjectId } = require('mongodb');
+const Contact = require('../src/Contact');
+
+const url = process.env.MONGODB_URL;
+const client = new MongoClient(url);
+
+async function run() {
+  await client.connect();
+  return 'Connected to the MongoDB server...';
+}
+
+run()
+  .then(console.log)
+  .catch(console.error);
+
+  const repo = {
+    findAll: async () => {
+      let contacts = [];
+      const contactColl = client.db('lab4').collection('contacts');
+      const cursor = contactColl.find({});
+      await cursor.forEach(row => {
+        console.log(row)
+        const contact = new Contact(row._id.toString(), row.firstName, row.lastName, row.emailAddress, row.notes, row.currentDate);
+        contacts.push(contact);
+      });
+      return contacts;
+    },
+
+    findById: async (uuid) => {
+        const contactColl = client.db('lab4').collection('contacts');
+        const filter = {
+          '_id': new ObjectId(uuid)
+        };
+        const row = await contactColl.findOne(filter);
+        console.log(row);
+        return new Contact(row._id.toString(), row.firstName, row.lastName, row.emailAddress, row.notes, row.currentDate);
+    },
+
+    create: async (contact) => {
+        const row = {firstName: contact.firstName, lastName: contact.lastName, emailAddress: contact.emailAddress, notes: contact.notes, currentDate: contact.currentDate};
+        const contactColl = client.db('lab4').collection('contacts');
+        const result = await contactColl.insertOne(row);
+        console.log(`A contact was inserted with the _id: ${result.insertedId}`);
+    },
+    
+    deleteById: async (uuid) => {
+        const contactColl = client.db('lab4').collection('contacts');
+        const filter = {
+          '_id': new ObjectId(uuid)
+        };
+        const result = await contactColl.deleteOne(filter);
+        if (result.deletedCount === 1) {
+          console.log("Successfully deleted a contact.");
+        } else {
+          console.log("No contact matched the query. Deleted 0 contact.");
+        }
+    },
+
+    update: async (contact) => { 
+        const contactColl = client.db('lab4').collection('contacts');
+        const filter = {
+          '_id': new ObjectId(contact.id)
+        };
+        const updateDoc = {
+          $set: {
+            firstName: contact.firstName,
+            lastName: contact.lastName,
+            emailAddress: contact.emailAddress,
+            notes: contact.notes,
+            currentDate: contact.currentDate
+          }
+        };
+        const result = await contactColl.updateOne(filter, updateDoc);
+        console.log(`${result.matchedCount} contact matched the filter, updated ${result.modifiedCount} contact(s)`);
+      },
+    };
+    
+    module.exports = repo;
---
+++ lab4-mongo-atlas/src/contactsRepository.js
@@ -0,0 +1,46 @@
+const db = new Map();
+const fs = require('fs');
+const path = require('path');
+const crypto = require('crypto');
+
+const saveData = () => {
+    const stringifyData = JSON.stringify(Array.from(db));
+    fs.writeFileSync(path.join(__dirname, '../data/contacts.json'), stringifyData);
+};
+
+const loadData = () => {
+    const fileData = fs.readFileSync(path.join(__dirname, '../data/contacts.json'));
+    const contactsArray = JSON.parse(fileData);
+    contactsArray.forEach(element => {
+        db.set(element[0], element[1]);
+    });
+};
+
+const repo = {
+    findAll: () => Array.from(db.values()),
+    findByID: (id) => db.get(id),
+    create: (contacts) => {
+        const newContact = {
+            id: crypto.randomUUID(),
+            firstName: contacts.firstName,
+            lastName: contacts.lastName,
+            email: contacts.email,
+            notes: contacts.notes,
+        };
+
+        db.set(newContact.id, newContact);
+        saveData();
+    },
+    deleteByID: (id) => {
+        db.delete(id);
+        saveData();
+    },
+    update: (contact) => {
+        db.set(contact.id, contact);
+        saveData();
+    },
+};
+
+loadData();
+
+module.exports = repo;
